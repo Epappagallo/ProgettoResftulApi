91b81016f2b828d66d4c018b1593090e24b283fc
Mattia <79059837+MattiDeP@users.noreply.github.com>
2025-11-04 10:12:01 +0000

    Aggiunta login e routes

--- Restful-Api-Project/src/App.tsx
+++ Restful-Api-Project/src/App.tsx
@@ -1,12 +1,21 @@
-import { useState } from 'react';
-import './App.css'
+import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import LoginPage from './pages/loginPage';
+import HomePage from './pages/homePage';
 
-function App() {
-  
+const App: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleNavigateToLogin = () => {
+    navigate('/login');
+  };
 
   return (
-    
-  )
-}
+    <Routes>
+      <Route path="/" element={<HomePage onNavigateToLogin={handleNavigateToLogin} />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
--- Restful-Api-Project/src/pages/homePage.tsx
+++ Restful-Api-Project/src/pages/homePage.tsx
@@ -1,19 +1,21 @@
 import react from 'react';
-import { HomePageProps } from '../types/pageProps';
+import type { HomePageProps } from '../types/pageProps';
 import styles from './homePage.module.css';
 
-const HomePage: React.FC<HomePagePros> = ({ onNavigateToLogin }) => {
+const HomePage: React.FC<HomePageProps> = ({ onNavigateToLogin }) => {
     return (
         <div className={styles.container}>
             <h1 className={styles.heading}>Benvenuto nella Home Page</h1>
             <p className={styles.description}>
                 Questa è la Home Page temporanea...
             </p>
-        <div className={styles.buttonContainer}>
-            <button className={styles.button} onClick={onNavigateToLogin}>
-                Vai al Login//
-            </button>
-        </div>
+            <div className={styles.buttonContainer}>
+                <button className={styles.loginButton} onClick={onNavigateToLogin}>
+                    Vai al Login
+                </button>
+            </div>
         </div>
     );
-}
+};
+
+export default HomePage;
