2430f05172ea929d6818fd3fae278ebe50e912e6
Edoardo Pappagallo <epappagallo@studenti.apuliadigitalmaker.it>
2025-11-04 10:51:41 +0100

    2nd commit

--- Restful-Api-Project/src/components/footer/footer.tsx
+++ Restful-Api-Project/src/components/footer/footer.tsx
@@ -1,8 +1,25 @@
-import react from 'react';
+// src/components/Footer/Footer.tsx
+import React from 'react';
 
+// Interfaccia opzionale, se il footer prendesse delle props (es. data copyright)
 interface FooterProps {
-    copyrightYear?: number;
+  copyrightYear?: number;
 }
 
 const Footer: React.FC<FooterProps> = ({ copyrightYear = new Date().getFullYear() }) => {
-    ret
+  return (
+    <footer style={{ 
+        marginTop: '80px', 
+        fontSize: '0.8em', 
+        color: '#aaa',
+        borderTop: '1px solid #eee',
+        paddingTop: '15px'
+    }}>
+      © {copyrightYear} Progetto React & TypeScript. Tutti i diritti riservati.
+    </footer>
+  );
+};
+
+export default Footer;
+
+// NOTA: I suoi stili andrebbero in un Footer.module.css accanto
