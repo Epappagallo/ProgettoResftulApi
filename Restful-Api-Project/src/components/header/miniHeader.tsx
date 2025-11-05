73d64e4199a6d3a4c42df26320855acc03f2c3ff
Edoardo Pappagallo <epappagallo@studenti.apuliadigitalmaker.it>
2025-11-05 06:39:50 +0100

    swag

--- Restful-Api-Project/src/components/header/miniHeader.tsx
+++ Restful-Api-Project/src/components/header/miniHeader.tsx
@@ -41,57 +41,64 @@ const MiniHeader: React.FC = () => {
                     <li className={styles.navItem} onClick={handleNoNavClick}>eBay Extra</li>
                     <li className={styles.navItem} onClick={handleNoNavClick}>eBay Imperdibili</li>
                     <li className={styles.navItem} onClick={handleNoNavClick}>Aiuto e contatti</li>
-                    <li className={styles.navItem} onClick={handleNoNavClick}>Vendi</li>
-
-                    {/* OGGETTI CHE OSSERVI - Menu a Tendina */}
-                    <li 
-                        className={`${styles.navItem} ${styles.dropdownContainer}`}
-                        onMouseEnter={() => handleMouseEnter(setIsWatchingOpen)}
-                        onMouseLeave={() => handleMouseLeave(setIsWatchingOpen)}
-                    >
-                        Oggetti che osservi <IoIosArrowDown className={styles.arrowIcon} />
-                        
-                        {isWatchingOpen && (
-                            <div className={styles.dropdownMenu}>
-                                <p className={styles.dropdownText}>
-                                    <Link to="/login" className={styles.blueLink}>Accedi</Link> per vedere tutti gli oggetti che stai osservando.
-                                </p>
-                            </div>
-                        )}
-                    </li>
-
-                    {/* IL MIO EBAY - Menu a Tendina */}
-                    <li 
-                        className={`${styles.navItem} ${styles.dropdownContainer}`}
-                        onMouseEnter={() => handleMouseEnter(setIsMyEbayOpen)}
-                        onMouseLeave={() => handleMouseLeave(setIsMyEbayOpen)}
-                    >
-                        Il mio eBay <IoIosArrowDown className={styles.arrowIcon} />
-
-                        {isMyEbayOpen && (
-                            <div className={styles.dropdownMenu}>
-                                <ul>
-                                    {myEbayItems.map((item) => (
-                                        <li key={item} className={styles.dropdownItem} onClick={handleNoNavClick}>
-                                            {item}
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        )}
-                    </li>
                 </ul>
 
-                {/* Sezione Icone a Destra */}
-                <div className={styles.iconContainer}>
-                    {/* Campanella (Notifiche) */}
-                    <div className={styles.iconWrapper} onClick={handleNoNavClick}>
-                        <IoMdNotificationsOutline size={20} className={styles.serviceIcon} />
-                    </div>
+                {/* Sezione a Destra: Vendi, Oggetti che osservi, Il mio eBay e Icone */}
+                <div className={styles.rightSection}>
+                    <ul className={styles.rightLinkList}>
+                        {/* VENDI */}
+                        <li className={styles.navItem} onClick={handleNoNavClick}>Vendi</li>
 
-                    {/* Carrello */}
-                    <div className={styles.iconWrapper} onClick={handleNoNavClick}>
-                        <IoMdCart size={20} className={styles.serviceIcon} />
+                        {/* OGGETTI CHE OSSERVI - Menu a Tendina */}
+                        <li 
+                            className={`${styles.navItem} ${styles.dropdownContainer}`}
+                            onMouseEnter={() => handleMouseEnter(setIsWatchingOpen)}
+                            onMouseLeave={() => handleMouseLeave(setIsWatchingOpen)}
+                        >
+                            Oggetti che osservi <IoIosArrowDown className={styles.arrowIcon} />
+                            
+                            {isWatchingOpen && (
+                                <div className={styles.dropdownMenu}>
+                                    <p className={styles.dropdownText}>
+                                        <Link to="/login" className={styles.blueLink}>Accedi</Link> per vedere tutti gli oggetti che stai osservando.
+                                    </p>
+                                </div>
+                            )}
+                        </li>
+
+                        {/* IL MIO EBAY - Menu a Tendina */}
+                        <li 
+                            className={`${styles.navItem} ${styles.dropdownContainer}`}
+                            onMouseEnter={() => handleMouseEnter(setIsMyEbayOpen)}
+                            onMouseLeave={() => handleMouseLeave(setIsMyEbayOpen)}
+                        >
+                            Il mio eBay <IoIosArrowDown className={styles.arrowIcon} />
+
+                            {isMyEbayOpen && (
+                                <div className={styles.dropdownMenu}>
+                                    <ul>
+                                        {myEbayItems.map((item) => (
+                                            <li key={item} className={styles.dropdownItem} onClick={handleNoNavClick}>
+                                                {item}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            )}
+                        </li>
+                    </ul>
+
+                    {/* Icone a Destra */}
+                    <div className={styles.iconContainer}>
+                        {/* Campanella (Notifiche) */}
+                        <div className={styles.iconWrapper} onClick={handleNoNavClick}>
+                            <IoMdNotificationsOutline size={20} className={styles.serviceIcon} />
+                        </div>
+
+                        {/* Carrello */}
+                        <div className={styles.iconWrapper} onClick={handleNoNavClick}>
+                            <IoMdCart size={20} className={styles.serviceIcon} />
+                        </div>
                     </div>
                 </div>
             </div>
